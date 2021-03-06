df0302a79b05e02b76c49f28730553f9e9b4c9b1
Natal <Natalliebarreto@outlook.co>
2021-03-05 20:38:27 -0500

    multiple rounds comeplete

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,9 @@ console.log(playerName);
 console.log(playerHealth, playerAttack, playerMoney);
 console.log("You can put regular sentences in these.");
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+enemyNames[0];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -69,4 +71,4 @@ var fight = function() {
 };
 
 // run fight function to start game
-fight();
+//fight();
