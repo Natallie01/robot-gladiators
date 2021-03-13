ae1ad8ba2b759c6e4adb4f81782164aa38e5cd14
Natal <Natalliebarreto@outlook.co>
2021-03-12 22:57:54 -0500

    update shop function to handle int inputs

--- assets/js/game.js
+++ assets/js/game.js
@@ -135,33 +135,29 @@ var fight = function(enemy) {
 };
 
 // go to shop between battles function
-var shop = function() {
-  // ask player what they'd like to do
-  var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
-  );
-
+var shopOptionPrompt = window.prompt(
+  "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+);
+  // convert answer from prompt to an actual number
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   // use switch case to carry out action
-  switch (shopOptionPrompt) {
-    case 'refill':
-    case 'REFILL':
-      playerInfo.refillHealth();
-      break;
-    case 'upgrade':
-    case 'UPGRADE':
-      playerInfo.upgradeAttack();
-      break;
-    case 'leave':
-    case 'LEAVE':
-      window.alert('Leaving the store.');
-      break;
-    default:
-      window.alert('You did not pick a valid option. Try again.');
-      shop();
-      break;
-  }
-};
-
+    switch (shopOptionPrompt) {
+      case 1:
+        playerInfo.refillHealth();
+        break;
+      case 2:
+        playerInfo.upgradeAttack();
+        break;
+      case 3:
+        window.alert("Leaving the store.");
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+    }
+  };
+  
 // function to set name
 var getPlayerName = function() {
   var name = "";
@@ -263,4 +259,4 @@ console.log(enemyInfo[0]['attack']);
 /* END GAME INFORMATION / VARIABLES */
 
 /* RUN GAME */
-startGame()};
+startGame();
